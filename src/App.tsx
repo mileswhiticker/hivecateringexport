ebc524623306b98a6671c8351032975723545c1b
Miles Whiticker <miles.whiticker@gmail.com>
2025-10-09 16:47:58 +1100

    feat(frontend): react now fetches the parsed spreadsheet data as a json object from the backend
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [count, setCount] = useState(0)
+    const backend_port = import.meta.env.VITE_HIVECATER_BACKEND_PORT || 4000;
+    const api_url = `http://localhost:${backend_port}/api/sheets`;
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch(api_url)
+            .then((res) => res.json())
+            .then((values) => setData(values))
+            .catch(console.error);
+    }, []);
 
   return (
     <>
@@ -28,6 +39,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <p>{data}</p>
     </>
   )
 }
