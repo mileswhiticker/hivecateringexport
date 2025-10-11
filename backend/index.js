96cd49c5d532efaa59ff7fe8d6870d9d73dca7d9
Miles Whiticker <miles.whiticker@gmail.com>
2025-10-11 12:59:00 +1100

    tweak: genericise and adjust some hosting settings to improve prod deployment
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -45,8 +45,9 @@ function get_uuid(){
 
 const sheets = google.sheets({ version: "v4", auth });
 
-const backend_port= process.env.VITE_HIVECATER_BACKEND_PORT || 4000;
-const backend_host= process.env.VITE_HIVECATER_BACKEND_HOST || "localhost";
+const backend_port = process.env.VITE_HIVECATER_BACKEND_PORT || 4000;
+const backend_host = process.env.VITE_HIVECATER_BACKEND_HOST || "localhost";
+const frontend_port = process.env.VITE_HIVECATER_FRONTEND_PORT || 5000;
 
 const api_url = `http://${backend_host}:${backend_port}/api/sheets`;
 const greeting_message = `Backend running on port ${backend_port}, access the raw Google Sheets data by going to <a href="${api_url}">${api_url}</a>
@@ -354,7 +355,7 @@ app.get("/api/sheets", async (req, res) => {
             parseDietPrefs(parsed_results[latestval + i], summed_results, daily_results, date_request);
         }
 
-        res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+        res.header("Access-Control-Allow-Origin", `http://localhost:${frontend_port}`);
         res.json(response_json);
     } catch (err) {
         api_status = "ERROR: Failed to read Google Sheets. Check the spreadsheet ID/s and sheet name/s in the environment |";
